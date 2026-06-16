538b4f9e7901fb19b9f8046764a4212c0ec567b3
Tuan Pham Anh <baabeetaa@gmail.com>
2026-06-16 11:37:01 +0700

    use repr u8 instead of consts (#10)

--- coins/src/transaction.rs
+++ coins/src/transaction.rs
@@ -2,11 +2,50 @@ use super::{Address, CoinId, CoinSpec, MultisigPolicy};
 use commonware_codec::{EncodeSize, Error, Read, ReadExt, Write};
 use nunchi_common::Operation;
 
-const OP_CREATE_TOKEN: u8 = 0;
-const OP_MINT: u8 = 1;
-const OP_BURN: u8 = 2;
-const OP_TRANSFER: u8 = 3;
-const OP_REGISTER_ACCOUNT_POLICY: u8 = 4;
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CoinOperationId {
+    CreateToken = 0,
+    Mint = 1,
+    Burn = 2,
+    Transfer = 3,
+    RegisterAccountPolicy = 4,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("invalid coin operation id: {0}")]
+pub struct InvalidCoinOperationId(u8);
+
+impl TryFrom<u8> for CoinOperationId {
+    type Error = InvalidCoinOperationId;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::CreateToken),
+            1 => Ok(Self::Mint),
+            2 => Ok(Self::Burn),
+            3 => Ok(Self::Transfer),
+            4 => Ok(Self::RegisterAccountPolicy),
+            _ => Err(InvalidCoinOperationId(value)),
+        }
+    }
+}
+
+impl Write for CoinOperationId {
+    fn write(&self, buf: &mut impl bytes::BufMut) {
+        buf.put_u8(*self as u8);
+    }
+}
+
+impl Read for CoinOperationId {
+    type Cfg = ();
+
+    fn read_cfg(buf: &mut impl bytes::Buf, _: &Self::Cfg) -> Result<Self, Error> {
+        let value = u8::read(buf)?;
+        Self::try_from(value)
+            .map_err(|_| Error::Invalid("CoinOperationId", "invalid operation id"))
+    }
+}
 
 /// A ledger operation authorized by a signed transaction.
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -40,22 +79,22 @@ impl Write for CoinOperation {
     fn write(&self, buf: &mut impl bytes::BufMut) {
         match self {
             Self::RegisterAccountPolicy { account_id, policy } => {
-                OP_REGISTER_ACCOUNT_POLICY.write(buf);
+                CoinOperationId::RegisterAccountPolicy.write(buf);
                 account_id.write(buf);
                 policy.write(buf);
             }
             Self::CreateToken { spec } => {
-                OP_CREATE_TOKEN.write(buf);
+                CoinOperationId::CreateToken.write(buf);
                 spec.write(buf);
             }
             Self::Mint { coin, to, amount } => {
-                OP_MINT.write(buf);
+                CoinOperationId::Mint.write(buf);
                 coin.write(buf);
                 to.write(buf);
                 amount.write(buf);
             }
             Self::Burn { coin, from, amount } => {
-                OP_BURN.write(buf);
+                CoinOperationId::Burn.write(buf);
                 coin.write(buf);
                 from.write(buf);
                 amount.write(buf);
@@ -66,7 +105,7 @@ impl Write for CoinOperation {
                 to,
                 amount,
             } => {
-                OP_TRANSFER.write(buf);
+                CoinOperationId::Transfer.write(buf);
                 coin.write(buf);
                 from.write(buf);
                 to.write(buf);
@@ -80,31 +119,30 @@ impl Read for CoinOperation {
     type Cfg = ();
 
     fn read_cfg(buf: &mut impl bytes::Buf, _: &Self::Cfg) -> Result<Self, Error> {
-        match u8::read(buf)? {
-            OP_REGISTER_ACCOUNT_POLICY => Ok(Self::RegisterAccountPolicy {
+        match CoinOperationId::read(buf)? {
+            CoinOperationId::RegisterAccountPolicy => Ok(Self::RegisterAccountPolicy {
                 account_id: Address::read(buf)?,
                 policy: MultisigPolicy::read(buf)?,
             }),
-            OP_CREATE_TOKEN => Ok(Self::CreateToken {
+            CoinOperationId::CreateToken => Ok(Self::CreateToken {
                 spec: CoinSpec::read(buf)?,
             }),
-            OP_MINT => Ok(Self::Mint {
+            CoinOperationId::Mint => Ok(Self::Mint {
                 coin: CoinId::read(buf)?,
                 to: Address::read(buf)?,
                 amount: u128::read(buf)?,
             }),
-            OP_BURN => Ok(Self::Burn {
+            CoinOperationId::Burn => Ok(Self::Burn {
                 coin: CoinId::read(buf)?,
                 from: Address::read(buf)?,
                 amount: u128::read(buf)?,
             }),
-            OP_TRANSFER => Ok(Self::Transfer {
+            CoinOperationId::Transfer => Ok(Self::Transfer {
                 coin: CoinId::read(buf)?,
                 from: Address::read(buf)?,
                 to: Address::read(buf)?,
                 amount: u128::read(buf)?,
             }),
-            tag => Err(Error::InvalidEnum(tag)),
         }
     }
 }
