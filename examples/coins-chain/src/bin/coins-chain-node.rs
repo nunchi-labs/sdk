e14b0d1d1ed1a58d78e19e79f0d9726a80ac0fd4
Tuan Pham Anh <baabeetaa@gmail.com>
2026-06-22 19:34:21 +0700

    feat: expose metrics (#70)

--- examples/coins-chain/src/bin/coins-chain-node.rs
+++ examples/coins-chain/src/bin/coins-chain-node.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use std::path::PathBuf;
-use tracing_subscriber::{fmt, EnvFilter};
 
 #[derive(Debug, Parser)]
 #[command(about = "Run a standalone coins-chain validator node")]
@@ -10,9 +9,6 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    fmt().with_env_filter(filter).init();
-
     let cli = Cli::parse();
     nunchi_coins_chain::testnet::run_node(cli.config)?;
     Ok(())
